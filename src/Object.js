07e9ec523a32f4530706ed45444968f438c57131
Jiadi Su <20302010043@fudan.edu.cn>
2023-05-23 17:55:07 +0800

    phong shading

--- src/Object.js
+++ src/Object.js
@@ -39,25 +39,48 @@ class ObjectLoader {
         attribute vec4 a_Position;
         attribute vec4 a_Color;
         attribute vec4 a_Normal;
+        uniform vec4 u_Eye;
         uniform mat4 u_MvpMatrix;
         uniform mat4 u_ModelMatrix;
-        uniform mat4 u_NormalMatrix;
+        varying vec4 v_Color;
+        varying float v_Dist;
+        varying vec4 v_Position;
+        varying vec4 v_Normal;
+        void main() {
+            gl_Position = u_MvpMatrix * a_Position;
+
+            v_Color = a_Color;
+            v_Dist = distance(u_ModelMatrix * a_Position, u_Eye);
+            v_Normal = a_Normal;
+            v_Position = u_ModelMatrix * a_Position;
+        }`;
+
+        // Fragment shader program
+        let FSHADER_SOURCE = `
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+        uniform vec3 u_FogColor;
+        uniform vec2 u_FogDist;
+        uniform vec4 u_EyeFrag;
         uniform vec3 u_Color;
-        uniform vec4 u_Eye;
+        uniform mat4 u_NormalMatrix;
         uniform vec3 u_LightDirection;
         uniform vec3 u_AmbientLight;
         uniform vec3 u_LightPosition;
         uniform vec3 u_PointLightColor;
         varying vec4 v_Color;
         varying float v_Dist;
+        varying vec4 v_Normal;
+        varying vec4 v_Position;
         void main() {
-            gl_Position = u_MvpMatrix * a_Position;
 
-            vec4 normal1 = u_NormalMatrix * a_Normal;
+            vec4 normal1 = u_NormalMatrix * v_Normal;
             vec3 normal = normalize(normal1.xyz);
-            vec4 vertexPostion = u_ModelMatrix * a_Position;
+
+            vec4 vertexPostion = v_Position;
             vec3 pointLightDirection = normalize(u_LightPosition - vec3(vertexPostion));
-            vec3 eyeDirection = normalize(u_Eye.xyz - vec3(vertexPostion));
+            vec3 eyeDirection = normalize(u_EyeFrag.xyz - vec3(vertexPostion));
             vec3 halfLE = normalize(u_LightDirection + eyeDirection);
             vec3 u_DiffuseLight = vec3(1.0, 1.0, 1.0);
 
@@ -69,23 +92,11 @@ class ObjectLoader {
             vec3 diffuse = u_DiffuseLight * u_Color * nDotL1 + u_PointLightColor * u_Color * nDotL2;
             vec3 ambient = u_AmbientLight * u_Color;
 
-            v_Color = vec4(specular + diffuse + ambient, a_Color.a);
-            v_Dist = distance(u_ModelMatrix * a_Position, u_Eye);
-        }`;
+            vec4 color =  vec4(specular + diffuse + ambient, v_Color.a);
 
-        // Fragment shader program
-        let FSHADER_SOURCE = `
-        #ifdef GL_ES
-        precision mediump float;
-        #endif
-        uniform vec3 u_FogColor;
-        uniform vec2 u_FogDist;
-        varying vec4 v_Color;
-        varying float v_Dist;
-        void main() {
             float fogFactor = clamp((u_FogDist.y - v_Dist) / (u_FogDist.y - u_FogDist.x), 0.0, 1.0);
-            vec3 color = mix(u_FogColor, vec3(v_Color), fogFactor);
-            gl_FragColor = vec4(color, v_Color.a);
+            vec3 colorAfterFog = mix(u_FogColor, vec3(color), fogFactor);
+            gl_FragColor = vec4(colorAfterFog, color.a);
         }`;
 
         // Initialize shaders
@@ -106,6 +117,7 @@ class ObjectLoader {
         this.u_ModelMatrix = this.gl.getUniformLocation(this.program, 'u_ModelMatrix');
 
         this.u_Eye = this.gl.getUniformLocation(this.program, 'u_Eye');
+        this.u_EyeFrag = this.gl.getUniformLocation(this.program, 'u_EyeFrag');
         this.u_FogColor = this.gl.getUniformLocation(this.program, 'u_FogColor');
         this.u_FogDist = this.gl.getUniformLocation(this.program, 'u_FogDist');
 
@@ -198,6 +210,7 @@ class ObjectLoader {
         // set eye
         var eyes = Camera.eye.elements;
         this.gl.uniform4f(this.u_Eye, eyes[0], eyes[1], eyes[2], 1.0);
+        this.gl.uniform4f(this.u_EyeFrag, eyes[0], eyes[1], eyes[2], 1.0);
         // set fog color and dist
         this.gl.uniform3fv(this.u_FogColor, new Vector3(fogColor).elements);
         this.gl.uniform2f(this.u_FogDist, fogDist[0], fogDist[1]);
